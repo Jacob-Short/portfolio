c72fa2a5b34e6d3b4f7700bd593be3cb73ffe0cb
Jacob-Short <thatmanjshort@gmail.com>
2021-10-27 21:33:19 -0700

    updated about me page

--- src/views/AboutMe.js
+++ src/views/AboutMe.js
@@ -5,16 +5,26 @@ export const AboutMe = () => {
     return (
         <Fragment>
             <div className='about-me-container'>
-            <h2>About Me</h2>
-            <h3>Education</h3>
-            <h4>Southern New Hampshire University</h4>
-            <h4>Kenzie Academy</h4>
-            <h4>Glencoe High School</h4>
-            <br></br>
-            <h3>Work Experience</h3>
-            <h4>Kenzie Academy -- Position</h4>
-            <h4>Singaore Math -- Position</h4>
-            <h4>Local 737 -- Position</h4>
+                <div>
+                    <h2>About Me</h2>
+                </div>
+                <div>
+                    <h3>Education</h3>
+                    <ul>
+                        <li><h4>Southern New Hampshire University</h4></li>
+                        <li><h4>Kenzie Academy</h4></li>
+                        <li><h4>Glencoe High School</h4></li>
+                    </ul>
+                </div>
+                <br></br>
+                <div>
+                    <h3>Work Experience</h3>
+                    <ul>
+                        <li><h4>Kenzie Academy -- Position</h4></li>
+                        <li><h4>Singaore Math -- Position</h4></li>
+                        <li><h4>Local 737 -- Position</h4></li>
+                    </ul>
+                </div>
             </div>
         </Fragment>
     )
